d76136391180c15df76a1c89e433d2c71bc764f0
Swastik <swastik713@gmail.com>
2020-09-17 13:12:37 +0530

    Power Function Implemented in Python

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -25,6 +25,9 @@ def divide(num1, num2):
 # Function to add power function
 #You cant use the inbuilt python function x ** y . Write your own function
 def power(num1, num2): #num1 ^ num2
+	#DivisionLogic 
+	if round(num2)!=num2:
+		return 0
 	if num2==0:
 		return 1
 	if num2==1:
@@ -39,24 +42,16 @@ def power(num1, num2): #num1 ^ num2
 #You cant use the inbuilt python function. Write your own function
 def printGP(a, r, n): 
 	gp=[]
-	for i in range(n):
-		gp.append(round(a*power(r,i),3))
 	return gp 
 
 # Python 3 program to print AP.  arithmetic Progression
 #You cant use the inbuilt python function. Write your own function
 def printAP(a, d, n): 
 	ap=[]
-	for i in range(n):
-		ap.append(round(a+i*d,3))
 	return ap
 
 # Python 3 program to print HP.   Harmonic Progression
 #You cant use the inbuilt python function. Write your own function
 def printHP(a, d, n): 
 	hp=[]
-	for i in range(n):
-		if a+i*d==0:
-			return 0
-		hp.append(round(1/(a+i*d),3))
 	return hp
