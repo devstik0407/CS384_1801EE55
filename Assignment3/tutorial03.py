b8d23a2acf934b87838784fc9234cf37d038d19d
Swastik <swastik713@gmail.com>
2020-10-15 21:55:47 +0530

    Defined a function to delete an existing analytics folder and recreate it

---
+++ Assignment3/tutorial03.py
@@ -0,0 +1,51 @@
+import os
+import csv
+import shutil
+
+def del_create_analytics_folder():
+    # del the analytics folder including subfolder
+    # mkdir the analytics folder (only mkdir)
+    if os.path.exists('analytics'):
+        shutil.rmtree(r'analytics')
+    os.mkdir('analytics')
+
+
+def course():
+    # Read csv and process
+    pass
+
+
+def country():
+    # Read csv and process
+    pass
+
+
+def email_domain_extract():
+    # Read csv and process
+    pass
+
+
+def gender():
+    # Read csv and process
+    pass
+
+
+def dob():
+    # Read csv and process
+    pass
+
+
+def state():
+    # Read csv and process
+    pass
+
+
+def blood_group():
+    # Read csv and process
+    pass
+
+
+# Create the new file here and also sort it in this function only.
+def new_file_sort():
+    # Read csv and process
+    pass
