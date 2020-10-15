3e7adf2f533ebe993ce9473c6ee42d21f3cd7323
Swastik <swastik713@gmail.com>
2020-10-15 21:57:23 +0530

    Created a function to check if a roll number is invalid

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -9,7 +9,28 @@ def del_create_analytics_folder():
         shutil.rmtree(r'analytics')
     os.mkdir('analytics')
 
+def inValidRollNo(rollNo):
+    if len(rollNo)!=8:
+        return True
+    if rollNo[0]<'0' and rollNo[0]>'9':
+        return True
+    if rollNo[1]<'0' and rollNo[1]>'9':
+        return True
+    if rollNo[2]<'0' or rollNo[2]>'2':
+        return True
+    if rollNo[3]<'1' or rollNo[3]>'2' or (rollNo[2]=='2' and rollNo[3]=='2'):
+        return True
+    branch={'CS','cs','EE','ee','ME','me','CB','cb','CE','ce','MT','mt','PH','ph','MA','ma','HS','hs','MA','ma','CH','ch','NT','nt'}
+    x = rollNo[4:6]
+    if x not in branch:
+        return True
+    if rollNo[6]<'0' and rollNo[6]>'9':
+        return True
+    if rollNo[7]<'0' and rollNo[7]>'9':
+        return True
+    return False
 
+    
 def course():
     # Read csv and process
     pass
