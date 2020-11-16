be912681513b9c200560d195736e7801dcd57cde
Swastik <swastik713@gmail.com>
2020-11-16 19:03:13 +0530

    Implemented a function to rename FIR files

--- Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment 5 Web Series Renamer/tutorial05.py
@@ -1,7 +1,21 @@
+import re
+import os
+seasonNumPadding = 3
+episodeNumPadding = 3
 
 def rename_FIR(folder_name):
-    # rename Logic 
-    
+    path = os.getcwd() + "\\Subtitles\\" + folder_name
+    listOfFiles = os.listdir(path)
+    for fileName in listOfFiles:
+        try:
+            text = re.findall('\d+',fileName)
+            episodeNum = (text[0]).strip()
+            extension = (re.split('\.',fileName)[-1]).strip()
+            if len(episodeNum) < episodeNumPadding :
+                episodeNum = '0'*(episodeNumPadding - len(episodeNum)) + episodeNum
+            os.rename(path+'\\'+fileName,path+'\\'+'FIR - Episode '+ episodeNum + '.' + extension)
+        except:
+            os.remove(path+'\\'+fileName)
 
 def rename_Game_of_Thrones(folder_name):
     # rename Logic 
